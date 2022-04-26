ea623b48e97abc3813e0407337fae026cd19aeb1
Ksenia Busquet <ksenia.busquet@gmail.com>
2022-04-26 10:08:43 -0300

    HTML structure, CSS applied

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,3 +1,18 @@
+// 1. One player 
+// 2. 16 cards in a 4x4 matrix
+// 3. Shuffle cards before start
+// 4. The game start after pressing the START button
+// 5. The game can be resetted by pressing the RESET button
+// 6. The timer starts with the game start
+// 7. The moves counter starts with the game start
+// 8. Player chooses 2 cards and tries to find 2 cards which are exactly the same
+// 9. If the cards are a match, theywill be kept flipped over
+// 10. If the cards are NOT a match, they will be flipped back again
+// 11. The game continues until all the the cards have been paired up
+// 12. BONUS: The "?" button will show the game rules
+
+let personMoves = 0;
+
 class MemoryGame {
     constructor(){
 
